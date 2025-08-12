37a44b4f5324c1a44d771beeac116bffed8ba70a
vbafnaa <vedicabafna@gmail.com>
2025-08-12 22:15:26 +0530

    timeline-changes

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -99,8 +99,8 @@ export function TimelineComp() {
           </p>
           <div className="grid grid-cols-1 gap-4">
             <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
+              src=""
+              alt="JIOTesseractImage"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
