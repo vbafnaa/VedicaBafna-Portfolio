1eb83c2e208d00d1b395ddd5d8c0bc47d461aa9e
vbafnaa <vedicabafna@gmail.com>
2025-08-13 19:05:34 +0530

    projects-changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
-            href="/"
+            href="https://github.com/vbafnaa"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             {/* <img
